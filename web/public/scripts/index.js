0a4575df8c37eebe6a3f4678fe5ecd32428dd4b0
Matias Davezac <matiasdavezac@gmail.com>
2024-11-10 23:06:55 -0300

    Upped cache time of assets and moved firebase comment to js file

--- web/public/scripts/index.js
+++ web/public/scripts/index.js
@@ -10,3 +10,17 @@ function headerSpaceHandler(event) {
 }
 addEventListener("load", headerSpaceHandler);
 addEventListener("resize", headerSpaceHandler);
+
+/*
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyA6QwLBu5X5AzgDq81C5L1dXZ9E0xP-9vk",
+  authDomain: "matiasdavezac-3d8c8.firebaseapp.com",
+  projectId: "matiasdavezac-3d8c8",
+  storageBucket: "matiasdavezac-3d8c8.firebasestorage.app",
+  messagingSenderId: "72369303819",
+  appId: "1:72369303819:web:528377b378f7a80d17f20e",
+  measurementId: "G-PTZ5232TNL"
+};
+const app = initializeApp(firebaseConfig);
+*/
